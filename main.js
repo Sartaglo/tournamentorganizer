c9734c88d7f75625bbf69200378cb6f215fbdf20
Sartaglo <sartaglo@outlook.com>
2020-12-06 04:35:59 -0700

    Update main.js to run bot

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
 "use strict";
 
-const { useDocs } = require("./use-docs");
-
-const tryUseDocs = async () => {
-    try {
-        return await useDocs();
-    } catch (error) {
-        console.error("Error using Docs:", error);
-
-        return null;
-    }
-}
+const readline = require("readline");
+const { runBot } = require("./run-bot");
 
 const main = async () => {
-    const docs = await tryUseDocs();
-
-    if (docs === null) {
-        return;
-    }
-
-    docs.documents.get(
-        { documentId: "1WipN6JeuMbRtMNn3iJngNJvqB8omr0Z2qWWuIDXuP1g" },
-        (error, response) => {
-            if (error) {
-                console.error("The API returned an error:", error);
-
-                return;
-            }
-
-            console.log(response.data.title);
+    const destroy = await runBot();
+    const readLineInterface = readline.createInterface(
+        {
+            input: process.stdin,
+            output: process.stdout,
+        },
+    );
+    readLineInterface.once(
+        "line",
+        () => {
+            destroy();
+            readLineInterface.close();
         },
     );
 };
