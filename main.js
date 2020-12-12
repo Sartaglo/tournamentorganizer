64b871f8e39090167dc6287a002fd6c09c67a203
Sartaglo <sartaglo@outlook.com>
2020-12-12 12:55:25 -0700

    Restructure Discord login to use environment variable

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
 "use strict";
 
-const readline = require("readline");
 const { runBot } = require("./run-bot");
 
-const main = async () => {
-    const destroy = await runBot();
-    const readLineInterface = readline.createInterface(
-        {
-            input: process.stdin,
-            output: process.stdout,
-        },
-    );
-    readLineInterface.once(
-        "line",
-        () => {
-            destroy();
-            readLineInterface.close();
-        },
-    );
-};
-
-main();
+runBot();
--- run-bot.js
+++ run-bot.js
@@ -1,44 +1,29 @@
 "use strict";
 
-const fs = require("fs");
 const { Client } = require("discord.js");
 const { actOnMessage } = require("./act-on-message");
 
 module.exports = {
-    runBot: () => {
-        return new Promise(
-            (resolve, reject) => {
-                fs.readFile(
-                    "discord-token.json",
-                    async (error, token) => {
-                        if (error) {
-                            reject("Error loading token: " + error);
+    runBot: async () => {
+        const client = new Client();
+        client.once(
+            "ready",
+            () => {
+                console.debug("Ready!");
+            },
+        );
+        client.on(
+            "message",
+            async (message) => {
+                await actOnMessage(client, message);
+            },
+        );
 
-                            return;
-                        }
-
-                        const client = new Client();
-                        client.once(
-                            "ready",
-                            () => {
-                                console.debug("Ready!");
-                            },
-                        );
-                        client.on(
-                            "message",
-                            async (message) => {
-                                await actOnMessage(message);
-                            },
-                        );
-                        await client.login(JSON.parse(token));
-                        resolve(
-                            () => {
-                                client.destroy();
-                            },
-                        );
-                    },
-                );
-            }
-        )
+        try {
+            await client.login(process.env.DISCORD_TOKEN);
+        } catch (error) {
+            console.error(error);
+            client.destroy();
+        }
     },
 };
